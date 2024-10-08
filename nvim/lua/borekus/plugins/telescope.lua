b513102de458ee4c215797059d1b8bd54eb9aeec
Arthur Lluisma <ajclluisma@gmail.com>
2024-10-09 00:47:51 +0800

    Nvim: remove trouble and modify telescope

--- nvim/lua/borekus/plugins/telescope.lua
+++ nvim/lua/borekus/plugins/telescope.lua
@@ -1,11 +1,14 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find grep string" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
+		vim.keymap.set("n", "<leader>fx", builtin.diagnostics, { desc = "Find diagnostics" })
+		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comments" })
 	end,
 }

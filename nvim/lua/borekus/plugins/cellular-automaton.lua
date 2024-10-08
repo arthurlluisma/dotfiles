8020fc680ac357bfb3023299f0a637aa68070b7d
Arthur Lluisma <ajclluisma@gmail.com>
2024-10-09 01:00:46 +0800

    Nvim: remove cell automaton and fugitive

--- nvim/lua/borekus/plugins/cellular-automaton.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"Eandrju/cellular-automaton.nvim",
-	keys = {
-		{ "<leader>mir", "<cmd>CellularAutomaton make_it_rain<CR>" },
-		{ "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>" },
-	},
-	cmd = "CellularAutomaton",
-}
